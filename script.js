769d03c8f8ef8208a63e6a90c0e08d327e4ca77b
belikeiram <iramm1159@gmail.com>
2024-04-30 21:35:29 +0530

    assignment

---
+++ script.js
@@ -0,0 +1,20 @@
+const card = document.querySelectorAll('.comp-card');
+
+
+
+// Array.from(card).forEach((item,i)=>{
+//     const cardLayer = item.querySelector('.card_layer');
+//     const title = item.querySelector('.title');
+//     const desc = item.querySelector('.desc');
+
+//     item.addEventListener('mouseover', ()=>{
+ 
+//         cardLayer.classList.add('active')
+        
+//     })
+//     item.addEventListener('mouseout', ()=>{
+//         cardLayer.classList.remove('active')
+        
+//     })
+// })
+
